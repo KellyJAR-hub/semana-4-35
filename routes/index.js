1b6cccb50f17fa12229d071a141ebe08e6b4b769
danvelandiav <vvmd25@gmail>
2020-12-18 17:37:21 -0500

    Rama final reto 4

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const routerx = require('express-promise-router');
+
+const apiRouterArticulo  = require('./api/articulo');
+const apiRouterUsuario   = require('./api/usuario')
+const apiRouterCategoria = require('./api/categoria')
+
+const router = routerx();
+
+router.use('/articulo', apiRouterArticulo);
+router.use('/categoria', apiRouterCategoria);
+router.use('/usuario', apiRouterUsuario);
+
+module.exports = router;
